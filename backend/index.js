7ca50f5e8b0a68abcc9f2953995a522a11acb9ad
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-04-03 04:01:04 +0300

    editing imdex.js file

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,36 @@
-const express=require('express');
-const app = express();
-app.get('/',(req,res)=> {
-res.send("hello world!!!!!");    
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authroutes = require('./routes/auth.routes');
+const path = require('path');const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routers
+app.use(authroutes);
+
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/elearning', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
 });
-app.listen(process.env.PORT, (err) => {
-    if (err) console.log(err);
-    console.log("Server is listining on port: ", process.env.PORT);
-    require("./configs/db.config");
-  });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
